52a1d95bf67cfcbee06efb43a717439e26fcdf7a
Oswin Gan <oswin.gan@gmail.com>
2022-10-09 11:10:56 -0400

    attempt 2

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -21,7 +21,9 @@ class BagMain {
         }
         if (double_enhance_handbags) {
             for (int i = 0; i < bags.length; i++) {
-                bags[i].enhance();
+                if (bags[i] instanceof  HandBag) {
+                    bags[i].enhance();
+                }
             }
         }
     }
